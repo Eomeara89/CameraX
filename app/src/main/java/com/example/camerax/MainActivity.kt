754720f9cd9fb216306ea31005ad01a6b7158c82
Cannibal89 <eddieomeara89@gmail.com>
2023-10-06 19:50:18 +0100

    Task-3 Hardware permissions

--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.camerax
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,11 +12,38 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.camerax.ui.theme.CameraXTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!hasRequiredPermissions()){
+            ActivityCompat.requestPermissions(
+                    this, CameraX_Permissions, 69
+                    )
+        }
+        setContent {
+            CameraXTheme {
+
+            }
+        }
+    }
+    //A function to check all permissions are granted
+    private fun hasRequiredPermissions(): Boolean{
+        return CameraX_Permissions.all{
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+    companion object{
+        private  val CameraX_Permissions = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        )
     }
 }
 
