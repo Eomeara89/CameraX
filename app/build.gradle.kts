b61911d7dbb7353bd0e63f8e5c041d15421b9993
Eddie O'Meara <105799277+Eomeara89@users.noreply.github.com>
2023-10-15 20:38:18 +0100

    Task-5 Adding button to flip camera between front and back.
    Also made a change to the SDK version to allow debugging on local device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.camerax"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.camerax"
-        minSdk = 29
+        minSdk = 33
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -7,13 +7,19 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -62,6 +68,21 @@ class MainActivity : ComponentActivity() {
                         ){
                             CameraPreview(controller = controller , modifier = Modifier
                                 .fillMaxSize())
+
+                        }
+                        IconButton(onClick = {
+                            controller.cameraSelector =
+                                if(controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA){
+                                    CameraSelector.DEFAULT_FRONT_CAMERA
+                                }else{
+                                    CameraSelector.DEFAULT_BACK_CAMERA
+                                }
+                        },
+                            modifier = Modifier
+                                .offset(16.dp, 16.dp)) {
+                            Icon(
+                                imageVector = Icons.Default.Cameraswitch ,
+                                contentDescription ="Switch the camera" )
 
                         }
 
